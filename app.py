5e77d075f1e003166517a2e690d20f0728bd8af7
Chris <166769576+gisthuband@users.noreply.github.com>
2025-01-23 18:45:52 -0800

    Add files via upload

--- app.py
+++ app.py
@@ -70,3 +70,84 @@ st.write(user_text1)
 
 user_text2 = st.text_area('input protein sequence 2')
 st.write(user_text2)
+
+seqs = [user_text1, user_text2]
+
+''
+''
+
+def seq_features(seqs):
+    
+    #structure = {'protein_1_seq':'p', 'protein_1_len':'p', '1_phobic_count':'p', '1_philic_count':'p', '1_basic_count': 'p', '1_acidic_count': 'p', '1_aromatic_count':'p', '1_sulfur_count': 'p', 'protein_2_seq':'p', 'protein_2_len':'p', '2_phobic_count':'p', '2_philic_count':'p', '2_basic_count': 'p', '2_acidic_count': 'p', '2_aromatic_count':'p', '2_sulfur_count': 'p'}
+    #new_df = pd.DataFrame(data=structure, index=[0])
+    
+        
+    p1_list = []
+    p2_list = []
+        
+    for i in range(2):
+            
+        amino_acids = ['A','C','D','E','F','G','H','I','K','L','M','N','P','Q', 'R','S','T','V','W','Y']
+    
+        ind_counts = {}
+        for a in amino_acids:
+            ind_counts[a] = 0
+
+        phobic = {'A':0, 'F':0, 'G':0, 'I':0, 'L':0, 'M':0, 'P':0, 'V':0, 'W':0, 'Y':0, 'phobic_total':0}
+        philic = {'C':0, 'D':0, 'E':0, 'H':0, 'K':0, 'N':0, 'Q':0, 'R':0, 'S':0, 'T':0, 'philic_total':0}
+        basic = {'H':0, 'K':0, 'R':0, 'basic_total':0}
+        acidic = {'D':0, 'E':0, 'acidic_total':0}
+        aromatic = {'F':0, 'H':0, 'W':0, 'Y':0, 'aromatic_total':0}
+        sulfur = {'C':0 , 'M':0, 'sulfur_total':0}
+            
+            
+        seq = seq[i]
+        seq_split = [y for y in seq]
+        seq_len = len(seq_split)
+            
+        for z in seq_split:
+                
+            if z in ind_counts:
+                ind_counts[z] += 1
+                    
+            if z in phobic:
+                phobic[z] += 1
+                phobic['phobic_total'] += 1
+                    
+            if z in philic:
+                philic[z] += 1
+                philic['philic_total'] += 1
+                    
+            if z in basic:
+                basic[z] += 1
+                basic['basic_total'] += 1
+                    
+            if z in acidic:
+                acidic[z] += 1
+                acidic['acidic_total'] += 1
+                    
+            if z in aromatic:
+                aromatic[z] += 1
+                aromatic['aromatic_total'] += 1
+                    
+            if z in sulfur:
+                sulfur[z] += 1
+                sulfur['sulfur_total'] += 1
+                    
+                    
+        if i == 0:
+                
+            p1_list = [seq, seq_len, phobic['phobic_total'], philic['philic_total'], basic['basic_total'], acidic['acidic_total'], aromatic['aromatic_total'], sulfur['sulfur_total']]
+                
+        elif i != 0:
+                
+            p2_list = [seq, seq_len, phobic['phobic_total'], philic['philic_total'], basic['basic_total'], acidic['acidic_total'], aromatic['aromatic_total'], sulfur['sulfur_total']]
+                
+    p3_list = p1_list + p2_list
+        
+        
+    return p3_list
+
+''
+''
+st.write(p3_list)
